1b15c3f18e031238535a56109a248f2befe907e2
zafarzhon <zafarzhon77@gmail.com>
2025-03-13 13:06:00 +0600

    refactored EF-48 Add new user

--- tests/GM/users/EF-48/Selectors.ts
+++ tests/GM/users/EF-48/Selectors.ts
@@ -1,22 +1,9 @@
 export const Selectors = {
   unitContainer: ".VehicleDashboard_vehicle_dashboard__ZZkoI",
-  addUnit: ".IconButton_open_block_cont__HN7q1",
-  threeDotsMenu2: ".css-q34dxg",
-  containsDelete: "Delete",
-  confirmDelete: "Delete",
   toastMessage: ".Toastify__toast-body",
   input: ".css-mnn31",
-  yearInput: '[name="year"]',
-  licenseInput: '[name="license_plate"]',
-  selectOption: ".css-p7gue6-option",
-  selectContainer: ".react-select__input-container",
-  nameInput: '[name="name"]',
-  vinInput: '[name="vin_sn"]',
   submitButton: 'button[type="submit"]',
   adminRole: ".UserAccessForm_block__i1ooN",
-  threeDotsMenu: ".css-1yxmbwk",
-  deleteButton: ".css-1km1ehz",
-  deleteConfirm: ".css-1hw9j7s",
 };
 export const loginSelectors = {
   email: 'input[type="text"]',
