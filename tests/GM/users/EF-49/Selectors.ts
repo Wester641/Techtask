b055691a6e11aa7777dd40a4fda33510b0b4a15a
Saadat23-4 <saadat.nurbek.ch@gmail.com>
2025-03-16 16:08:10 -0500

    Added EF-49 test case for users tab functionality

---
+++ tests/GM/users/EF-49/Selectors.ts
@@ -0,0 +1,13 @@
+export const Selectors = {
+  searchInput: ".SearchFiters_containerSearch__searchBox__UwgF2",
+  users: "[class='Sidebar_sidebarItem__2s00-']",
+  usersSidebarPopup: ".Sidebar_sidebarItem__popup__b7axi",
+  selectUsersPopup: ".Sidebar_sidebarItem__popup_item__8NCQT",
+  userTabs: "[class*='css-1q2h7u5']",
+  activeTab: "[class*='css-1q2h7u5']",
+};
+export const loginSelectors = {
+  email: 'input[type="text"]',
+  password: 'input[type="password"]',
+  submitButton: 'button[type="submit"]',
+};
