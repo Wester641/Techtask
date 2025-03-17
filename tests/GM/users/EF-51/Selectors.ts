b2ffcbe82cff4882ad8c4495c9c0e8ddc24d11af
Saadat23-4 <saadat.nurbek.ch@gmail.com>
2025-03-16 21:58:01 -0500

    Added EF-51 test case

---
+++ tests/GM/users/EF-51/Selectors.ts
@@ -0,0 +1,24 @@
+export const Selectors = {
+  searchInput: ".SearchFiters_containerSearch__searchBox__UwgF2",
+  users: "[class='Sidebar_sidebarItem__2s00-']",
+  threeDotsBtn: "[class*='Meatballs_button__wNC2']",
+  threeDotsMenu: "ul[class*='Meatballs_menu__F9Dnr css-r8u8y9']",
+  threeDotsMenuItem: "[class*='ss-1km1ehz']",
+  basicDetailsText: ".UserForm_form__label__vVIl8",
+  input: ".css-mnn31",
+  userTabs: "[class*='css-1q2h7u5']",
+  activeTab: "[class*='css-1q2h7u5']",
+  submitBtn: '[type="submit"]',
+  deleteBtn: "[class*='ConfirmModal_delete-btn']",
+  confirmDelText: ".ConfirmModal_title__0Iijs",
+  toastMsg: ".Toastify__toast-body",
+  deleteToastMsg: "[class*='Toastify__toast-icon']",
+};
+export const loginSelectors = {
+  email: 'input[type="text"]',
+  password: 'input[type="password"]',
+  submitButton: 'button[type="submit"]',
+};
+
+export const today = new Date().toDateString();
+export const time = new Date().toLocaleTimeString();
