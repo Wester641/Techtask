195689d698a2669b50c606fbaaacb9324f7c568f
zafarzhon <zafarzhon77@gmail.com>
2025-03-20 13:59:38 +0600

    Added [EF-114 EF-115 EF-116] cases in samsara devices

---
+++ tests/GM/samsara-devices/EF-115/Selectors.ts
@@ -0,0 +1,4 @@
+export const Selectors = {
+  perPageButton: ".Pagination_paginationCont__range__perPage__option__6SSOl",
+  selectCount: ".css-12g4q3m",
+};
---
+++ tests/GM/samsara-devices/EF-116/Selectors.ts
@@ -0,0 +1,3 @@
+export const Selectors = {
+  refreshButton: ".css-1yxmbwk",
+};
