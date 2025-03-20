977b525b8375a0a9b6330c47134025fae520d5bb
zafarzhon <zafarzhon77@gmail.com>
2025-03-20 15:18:36 +0600

    Fixed the screenSize first entry and changed search the button and click

--- tests/GM/units/EF-156/Selectors.ts
+++ tests/GM/units/EF-156/Selectors.ts
@@ -1,3 +1,4 @@
 export const Selectors = {
   unit_cell: ".MuiTableBody-root .MuiTableRow-root",
+  addButton: ".css-1yxmbwk",
 };
--- tests/GM/units/EF-158/select_add_work_order.spec.ts
+++ tests/GM/units/EF-158/select_add_work_order.spec.ts
@@ -1,17 +1,23 @@
 import { test, expect } from "@playwright/test";
 import { Selectors } from "./Selectors";
 
-import { URLs } from "../../../../constants/links";
+import { screenSize, URLs } from "../../../../constants/links";
 
 test("EF-158__Select add work order", async ({ page }) => {
+  await page.setViewportSize(screenSize);
   await page.goto(URLs.units);
+
+  // waiting for unit list to load
   await page.waitForSelector(Selectors.unit_cell, {
     state: "attached",
     timeout: 10000,
   });
-  await page.locator(Selectors.unit_cell).nth(1).click();
-  await page.getByRole("button").filter({ hasText: /^$/ }).nth(1).click();
+
+  // clicking on first unit
+  await page.locator(Selectors.unit_cell).nth(0).click();
+  await page.locator(Selectors.addButton).nth(1).click();
   await page.getByRole("link", { name: "Add Work Order" }).click();
 
   await expect(page).toHaveURL(URLs.addWorkOrderFromUnitPage);
+  await page.waitForTimeout(2000);
 });
