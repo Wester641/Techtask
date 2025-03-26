09e31e3ee90ab0e01aca524563dbe44acf8ef522
billkasius <ganter.eugene@gmail.com>
2025-03-26 16:04:13 +0600

    EF-129 EF-130 | Settings section

---
+++ tests/GM/settings/EF-130/Selectors.ts
@@ -0,0 +1,15 @@
+export const Selectors = {
+  tabNavigation: ".InnerSidebar_content__display__item__Xtaxl",
+  searchInput: ".css-b52kj1",
+  settingsButton: ".css-1hw9j7s",
+  infoBlock: ".SettingsPagination_container__counts__tm2jY",
+  dataResult: ".RoleList_list__item__FCUkw",
+  addButton: ".IconButton_label_block__Ri\\+VB",
+  createRoleModal: ".css-mbdu2s",
+  inputField: ".Inputs_inputs__WMLTN",
+  tickHeader: ".ModuleForm_tree_headers__HUpdz",
+  tickRow: ".PermissionForm_permission__VKMRr",
+  permSelect: ".css-p58oka"
+
+  };
+  
