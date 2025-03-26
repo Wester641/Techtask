26f67a880ac592200d4ecc71f1bc6743ec725a22
zafarzhon <zafarzhon77@gmail.com>
2025-03-26 17:02:17 +0600

    Added [EF-124, EF-125, EF-126] cases

---
+++ tests/GM/papers/EF-125/Selectors.ts
@@ -0,0 +1,7 @@
+export const Selectors = {
+  filter_widget: ".ModalOptions_blockFiltering__jpAKN",
+  modal_filter:
+    ".ModalOptions_blockFiltering__trueModal__inModal__paddingModal__9uCpm",
+  list_item:
+    ".ModalOptions_blockFiltering__trueModal__inModal__paddingModal__listItem__-6JIk",
+};
