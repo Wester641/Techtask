bb3d46537b2efedd07fe79e8462229750c63f0e6
billkasius <ganter.eugene@gmail.com>
2025-03-28 17:26:40 +0600

    EF-200 EF-201 | Inspection Form

---
+++ tests/SM/inspection-form/EF-203/Selectors.ts
@@ -0,0 +1,10 @@
+export const Selectors = {
+  settingsButton: ".css-1yxmbwk",
+  settingsDropdown: ".css-pwxzbm",
+  subButton: ".css-1km1ehz",
+  deleteModal: ".ConfirmModal_content__PbUvt",
+  deleteModalButton: ".css-1hw9j7s",
+  cellTitle: ".InspectionsFormsList_renderCell__title__te5Zz",
+  headerTable: ".css-1gqug66"
+
+};
