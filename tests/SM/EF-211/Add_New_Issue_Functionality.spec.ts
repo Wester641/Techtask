4ec56578072744af164a186cdb7ca5b2f25bebef
ozodbek <ozodbek200017@gmail.com>
2025-04-01 14:33:59 +0600

    Added [EF-210, EF-211]

--- tests/SM/EF-211/Add_New_Issue_Functionality.spec.ts
+++ tests/SM/EF-211/Add_New_Issue_Functionality.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
-// import { Selectors } from "./Selectors";
 import { screenSize } from "../../../constants/links";
+import { today } from "./Selectors";
 
 const randomOption = Math.floor(Math.random() * 3);
 
@@ -13,45 +13,20 @@ test("EF-211__Add New Issue Functionality", async ({ page }) => {
   await page.locator(".react-select__control").first().click();
   await page.getByRole("option").nth(randomOption).click();
 
-  await page
-    .locator(
-      "div:nth-child(2) > .SelectField_select__nkrpi > .Select_select__input__6V5DK > .react-select__control > .react-select__value-container"
-    )
-    .click();
-  await page.getByRole("option").nth(randomOption).click();
+  for (let i = 0; i < 5; i++) {
+    await page.locator(".css-19bb58m").nth(i).click();
+    await page.getByRole("option").nth(randomOption).click();
+  }
 
   await page
     .locator('input[name="summary"]')
-    .fill(".SelectField_select__nkrpi > .Select_select__input__6V5DK > ");
-
-  await page.locator('textarea[name="description"]').fill("gggggg");
+    .fill(`This summary added ${today}`);
 
   await page
-    .locator(
-      "div:nth-child(7) > .SelectField_select__nkrpi > .Select_select__input__6V5DK > .react-select__control"
-    )
-    .click();
-  await page.getByRole("option").nth(randomOption).click();
-
-  // ll
-
-  await page
-    .locator(
-      "div:nth-child(8) > .SelectField_select__nkrpi > .Select_select__input__6V5DK > .react-select__control"
-    )
-    .click();
-  await page.getByRole("option").nth(randomOption).click();
-
-  await page
-    .locator(
-      "div:nth-child(9) > .SelectField_select__nkrpi > .Select_select__input__6V5DK > .react-select__control"
-    )
-    .click();
-  await page.getByRole("option").nth(randomOption).click();
+    .locator('textarea[name="description"]')
+    .fill(`This decsription added ${today}`);
 
   await page.getByRole("button", { name: "Save" }).click();
 
   await page.waitForTimeout(3000);
 });
-
-// await page.locator(Selectors.searchInput).nth(0).fill("Ben");
