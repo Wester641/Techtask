2d0e1d77454901b197ad6ec199f0f4545b199f49
zafarzhon <zafarzhon77@gmail.com>
2025-04-02 12:02:39 +0600

    commented the unwork style logic

--- tests/SM/inspections/EF-193/page_navigation.spec.ts
+++ tests/SM/inspections/EF-193/page_navigation.spec.ts
@@ -1,30 +1,11 @@
 import { test, expect } from "@playwright/test";
 import { Selectors } from "./Selectors";
-import { Styles, URLs, screenSize } from "../../../../constants/links";
+import { URLs, screenSize } from "../../../../constants/links";
 
 test("EF-193__Inspections History Page Navigation", async ({ page }) => {
   await page.setViewportSize(screenSize);
 
   await page.goto(URLs.inspection_history);
-
-  await page.waitForTimeout(500);
-
-  for (let i = 0; i < 3; i++) {
-    const sideBar = await page.getByRole("heading").nth(i);
-
-    await sideBar.evaluate((element) => {
-      element.style.backgroundColor = Styles.background_color;
-      element.style.border = Styles.border;
-    });
-
-    await page.waitForTimeout(500);
-
-    await sideBar.evaluate((element) => {
-      element.style.backgroundColor = Styles.transparent;
-      element.style.border = Styles.none;
-    });
-    expect(sideBar).toBeVisible();
-  }
 
   await page.waitForTimeout(3000);
 
