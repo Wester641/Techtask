dd0b2b22686a238727e87097593f8f59e7316027
zafarzhon <zafarzhon77@gmail.com>
2025-04-02 12:28:19 +0600

    refactored all columns

---
+++ tests/SM/inspections/inspection-form/EF-200/page_navigation.spec.ts
@@ -0,0 +1,60 @@
+import { test, expect } from "@playwright/test";
+import { Selectors } from "./Selectors";
+import { screenSize, URLs } from "../../../../../constants/links";
+
+test("EF-200__Inspections Form Page Navigation", async ({ page }) => {
+  await page.setViewportSize(screenSize);
+
+  await page.goto(URLs.inspectionForms);
+
+  await page.waitForTimeout(500);
+
+  await page.addStyleTag({
+    content: `
+      ${Selectors.managementTab} {
+        background-color: #7d9ec087 !important; 
+        border: 1px solid #7d9ec087 !important;      
+      }`,
+  });
+
+  await page.waitForTimeout(500);
+
+  // Remove styling
+  await page.addStyleTag({
+    content: `
+      ${Selectors.managementTab} {
+        background-color: transparent !important;
+        border: none !important;
+      }`,
+  });
+
+  await page.waitForTimeout(3000);
+
+  await page.locator(Selectors.managementTab).nth(1).click();
+
+  await page.waitForTimeout(1000);
+
+  await page
+    .locator("div")
+    .filter({ hasText: /^Inspections$/ })
+    .click();
+
+  await expect(page.locator(Selectors.selectDropdown)).toBeVisible();
+
+  await page.waitForTimeout(500);
+
+  await page.locator(Selectors.inspectionsTab).nth(1).click();
+
+  await page.waitForTimeout(3000);
+
+  await expect(page.locator(Selectors.headerTable).first()).toBeVisible();
+
+  const headerNames = await page
+    .locator(Selectors.headerTable)
+    .first()
+    .allInnerTexts();
+
+  await expect(headerNames).toStrictEqual([
+    "Title\tDescription\tItem\tSubmissions\t",
+  ]);
+});
---
+++ tests/SM/issues/EF-210/navigate_to_the_issues_page.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from "@playwright/test";
+import { screenSize } from "../../../../constants/links";
+
+test("EF-210__Navigate to the issues page", async ({ page }) => {
+  await page.setViewportSize(screenSize);
+
+  await page.goto("/units");
+
+  await page
+    .locator("div")
+    .filter({ hasText: /^Service management$/ })
+    .click();
+
+  await page
+    .locator("div")
+    .filter({ hasText: /^Issue$/ })
+    .click();
+
+  await page.getByRole("listitem").filter({ hasText: "Issue" }).click();
+  await expect(
+    page
+      .locator("div")
+      .filter({ hasText: /^Issue Assign To$/ })
+      .nth(1)
+  ).toBeVisible();
+
+  await expect(
+    page
+      .locator("div")
+      .filter({ hasText: /^Asset name$/ })
+      .nth(1)
+  ).toBeVisible();
+
+  await expect(
+    page
+      .locator("div")
+      .filter({ hasText: /^Asset name$/ })
+      .nth(1)
+  ).toBeVisible();
+
+  await expect(
+    page
+      .locator("div")
+      .filter({ hasText: /^Labels$/ })
+      .nth(1)
+  ).toBeVisible();
+
+  await expect(
+    page
+      .locator("div")
+      .filter({ hasText: /^Priority$/ })
+      .nth(1)
+  ).toBeVisible();
+});
