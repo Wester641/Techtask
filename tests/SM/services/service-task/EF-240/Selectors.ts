f7f05004d7a9fec3f7c4ffeb01a7da1becc92e99
zafarzhon <zafarzhon77@gmail.com>
2025-04-02 17:46:45 +0600

    updated [EF-239, EF-240] cases

--- tests/SM/services/service-task/EF-240/Selectors.ts
+++ tests/SM/services/service-task/EF-240/Selectors.ts
@@ -4,6 +4,8 @@ export const Selectors = {
   create_service_task_form: ".Block_block__U3GqW",
   input: ".css-mnn31",
   description: ".css-10oer18",
+  three_dot_menu: ".css-pwxzbm",
+  confirm_delete_modal: ".ConfirmModal_content__PbUvt",
 };
 
 export const today = new Date().toDateString();
