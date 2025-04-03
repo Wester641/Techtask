2ca443edbc6e25598321971e0861cea521fa85e0
ozodbek <ozodbek200017@gmail.com>
2025-04-03 17:55:10 +0600

    EF-105  translate Selectors

--- tests/SM/issues/EF-220/servicehistory_functionality.spec.ts
+++ tests/SM/issues/EF-220/servicehistory_functionality.spec.ts
@@ -1,24 +1,18 @@
 import { test } from "@playwright/test";
-
+import { Selectors } from "./Selectors";
 import { screenSize } from "../../../../constants/links";
-
 const randomOption = Math.floor(Math.random() * 3);
 
-test("EF-220__Add Service History Functionality", async ({ page }) => {
+test("EF-220__ service history functionality", async ({ page }) => {
   await page.setViewportSize(screenSize);
 
   await page.goto("/service-history");
-  await page.locator(".IconButton_label_block__Ri\\+VB").click();
+  await page.locator(Selectors.icon_button).click();
 
-  await page.locator(".react-select__value-container").first().click();
+  await page.locator(Selectors.select_container).first().click();
   await page.getByRole("option").nth(randomOption).click();
 
-  await page
-    .locator(
-      "div:nth-child(2) > .SelectField_select__nkrpi > .Select_select__input__6V5DK > .react-select__control"
-    )
-    .click();
+  await page.locator(Selectors.select_field2).click();
   await page.getByRole("option").nth(randomOption).click();
-
   await page.getByRole("button", { name: "Save Service Entry" }).click();
 });
