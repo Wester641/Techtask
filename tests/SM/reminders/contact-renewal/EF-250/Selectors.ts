07d172200b7323f3ef4f42d5ce97865cb8dca1bb
billkasius <ganter.eugene@gmail.com>
2025-04-08 12:11:45 +0600

    EF-250 EF-252 | Finished (upgrade EF-250 asap)

---
+++ tests/SM/reminders/contact-renewal/EF-250/Selectors.ts
@@ -0,0 +1,9 @@
+export const Selectors = {
+    dataRow: ".css-1liixou",
+    detailsModal: ".DetailInfo_detail_withMaxMinWith__r6B1V",
+    settingsButton: ".css-mrqjss",
+    textInput: ".css-10oer18",
+    dataCell: ".css-q34dxg",
+    selectButton: ".css-hlgwow",
+    numberField: ".css-mnn31"
+  };  
