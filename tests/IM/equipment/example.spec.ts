6f97ec5f453009ad17e5981d6dce12facb9ebacc
zafarzhon <zafarzhon77@gmail.com>
2025-04-21 10:57:06 +0600

    refactored 21.04 by zafar

--- tests/IM/equipment/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from "@playwright/test";
-import { screenSize } from "../../../constants/links";
-
-// delete this file if this unusual
-test("exmaple-723", async ({ page }) => {
-  await page.setViewportSize(screenSize);
-
-  await page.goto("/units");
-});
--- tests/IM/part/parts/EF-255/Selectors.ts
+++ tests/IM/part/parts/EF-255/Selectors.ts
@@ -1,31 +1,10 @@
 export const Selectors = {
-  addPartButton: ".IconButton_label_block__Ri\\+VB",
-  partNumberInput: 'input[name="part_number"]',
-  descriptionInput: 'textarea[name="description"]',
-  manufacturerPartNumberInput: 'input[name="manufacturer_part_number"]',
-  upcInput: 'input[name="upc"]',
-  unitCostInput: 'input[name="unit_cost"]',
-
-  firstDropdown: ".react-select__value-container",
-  fifthDropdown:
-    "div:nth-child(5) > .Select_select__input__6V5DK > .react-select__control > .react-select__value-container",
-  manufacturerDetailsDropdown:
-    ".Form_manufacturer_details__kz9Gj > .SelectField_select__nkrpi > .Select_select__input__6V5DK > .react-select__control > .react-select__value-container",
-  secondFormDropdown:
-    ".Form_form__0FC7z > div:nth-child(2) > .SelectField_select__nkrpi > .Select_select__input__6V5DK > .react-select__control > .react-select__value-container",
-
-  locationAisleInput: 'input[name="locations\\[0\\]\\.aisle"]',
-  locationRowInput: 'input[name="locations\\[0\\]\\.row"]',
-  locationBinInput: 'input[name="locations\\[0\\]\\.bin"]',
-
-  addVendorButton: { text: /^Add Vendor$/ },
-
-  purchaseVendorDropdown:
-    ".PurchaseHistory_purchaseHistoryInput__Efv4V > .SelectField_select__nkrpi > .Select_select__input__6V5DK > .react-select__control > .react-select__value-container",
-
-  purchaseQuantityInput: { role: "textbox", name: "0" },
-  chooseDateButton: { role: "button", name: "Choose date" },
-  dateGridCell20: { role: "gridcell", name: "20" },
-
-  saveButton: { role: "button", name: "Save" },
+  part_number_input: 'input[name="part_number"]',
+  description_input: 'textarea[name="description"]',
+  select_field: ".react-select__input-container",
+  input: ".css-mnn31",
 };
+
+export const today = new Date().toDateString();
+export const todayForFill = new Date();
+export const time = new Date().toLocaleTimeString();
