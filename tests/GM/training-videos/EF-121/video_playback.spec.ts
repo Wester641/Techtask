bfdd5075d6acd6c85d0ced609a2523d0da606062
billkasius <ganter.eugene@gmail.com>
2025-04-22 14:41:23 +0600

    EF-121 Simplified and Fixed

--- tests/GM/training-videos/EF-121/video_playback.spec.ts
+++ tests/GM/training-videos/EF-121/video_playback.spec.ts
@@ -7,35 +7,13 @@ test("EF-121__Verify Video Playback Functionality", async ({ page }) => {
 
   await page.goto(URLs.trainingPage, { waitUntil: 'networkidle' });
 
-  await page.waitForTimeout(1000);
-
-        // Add styling
-  await page.addStyleTag({
-          content: `
-          ${Selectors.watchButton} {
-            background-color: #7d9ec087 !important; 
-            border: 1px solid #7d9ec087 !important;      
-          }`,
-        });
-      
-  await page.waitForTimeout(500);
-      
-        // Remove styling
-  await page.addStyleTag({
-        content: `
-          ${Selectors.watchButton} {
-            background-color: transparent !important;
-            border: none !important;
-          }`,
-    });
+  await page.waitForTimeout(3000);
     
-  await page.waitForTimeout(1000);
-
   await page.locator(Selectors.watchButton).nth(0).click();
 
   await page.waitForTimeout(3000);
 
-  await expect(page.locator(Selectors.playButton).nth(3)).toBeVisible;
+  expect(page.locator(Selectors.playButton).nth(3)).toBeVisible;
 
-  await page.locator(Selectors.closeButton).click();
+  await page.locator(Selectors.closeButton).nth(1).click();
 });
