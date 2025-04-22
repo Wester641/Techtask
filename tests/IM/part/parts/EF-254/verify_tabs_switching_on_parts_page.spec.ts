09a88a0837e82aaa23eb0d0f2d52d0361734b538
zafarzhon <zafarzhon77@gmail.com>
2025-04-22 12:58:35 +0600

    refactored [EF-261 and EF-262] cases

--- tests/IM/part/parts/EF-254/verify_tabs_switching_on_parts_page.spec.ts
+++ tests/IM/part/parts/EF-254/verify_tabs_switching_on_parts_page.spec.ts
@@ -5,8 +5,9 @@ import { Selectors } from "./Selectors";
 test("EF-254__verify_tabs_switching_on_parts_page", async ({ page }) => {
   await page.setViewportSize(screenSize);
   await page.goto(URLs.partsPage);
-  for (let i = 1; i < 4; i++) {
+  for (let i = 1; i < 3; i++) {
     await page.getByRole("tab").nth(i).click();
     await expect(page.locator(Selectors.mainContainer)).toBeVisible();
   }
+  await page.getByRole("tab", { name: "Archived" }).click();
 });
