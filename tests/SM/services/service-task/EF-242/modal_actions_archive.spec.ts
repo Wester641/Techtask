05c0189ea4cfcb91a51c94b0f13904e00873b90b
ozodbek <ozodbek200017@gmail.com>
2025-04-23 12:49:49 +0600

    EF-242  I couldn't find the button

--- tests/SM/services/service-task/EF-242/modal_actions_archive.spec.ts
+++ tests/SM/services/service-task/EF-242/modal_actions_archive.spec.ts
@@ -23,7 +23,12 @@ test("EF-242__Test functionality of the archiving service template and unarchive
   await page.waitForTimeout(5000);
   await page.getByRole("cell").nth(0).click();
   // expect(page.locator(Selectors.actions_modal).nth(1)).toBeVisible();
-  await page.locator(Selectors.icon_button).nth(3).click();
+
+  await page
+    .locator(".DetailInfo_detail__header__icons__FORVB svg.css-mrqjss")
+    .nth(1)
+    .click();
+
   expect(page.getByText("Task successfully unarchived!")).toBeVisible();
   await page.getByRole("tab", { name: "Active" }).click();
 });
