cc83f7d05184b69d1cadae6c0b8f21ebdb390d6d
billkasius <ganter.eugene@gmail.com>
2025-04-23 15:14:42 +0600

    EF-46 Fix

--- constants/links.ts
+++ constants/links.ts
@@ -8,6 +8,7 @@ export const URLs = {
   dashboard: "/dashboard",
   units: "/units",
   unitsFirstPage: "/units?page=1",
+  unitCreate: "/units/create",
   users: "/users",
   fuelHistory: "/fuel-history",
   createFuelHistory: "/fuel-history/create",
